ea380375c492d0e3cb822d47cbeb704fcfd48a07
Olarewaju Damilare <damilareolarewaju@yahoo.com>
2024-12-17 21:49:49 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="relative flex flex-col items-center justify-center h-screen bg-[#EFEFEF] px-4">
+      <h1>under consruction </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
