02296e825b6b2c62e16e2a982d05ff716b0571fe
CheeryCodes <esthernehemiah57@gmail.com>
2024-12-28 23:48:25 +0100

    my commit

---
+++ src/layout/header/TopDesign.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const TopDesign = () => {
+  return (
+    <div className="top-design">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="432"
+        height="48"
+        viewBox="0 0 432 48"
+        fill="none"
+      >
+        <path
+          d="M0 0H432C432 34.1392 397.151 57.2012 365.728 43.8571L361.203 41.9355C266.6 1.76177 159.632 2.30146 65.4397 43.4277C34.5642 56.9086 0 33.6903 0 0Z"
+          fill="url(#paint0_linear_161_1261)"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear_161_1261"
+            x1="216"
+            y1="0"
+            x2="216"
+            y2="72"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#D4E3FB" />
+            <stop offset="1" stopColor="#A3C5F8" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+  );
+};
+
+export default TopDesign;
