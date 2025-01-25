e917233fb595d4af3617b87cdca3e5846211c94d
Solomon Zion <127247997+brainycodes@users.noreply.github.com>
2025-01-25 11:36:46 -0900

    Update axiosInstance.js

--- src/backend/api/axiosInstance.js
+++ src/backend/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://lantern-pro.vercel.app/api',
 });
 
 export default axiosInstance;
