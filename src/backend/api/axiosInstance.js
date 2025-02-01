8ea50117f5dd3a387c6a74429198e530d992d6eb
Solomon Zion <brainycodes.tech@gmail.com>
2025-02-01 04:39:26 -0800

    updating

--- src/backend/api/axiosInstance.js
+++ src/backend/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  baseURL: 'https://lantern.academy/backend'
+  baseURL: 'https://lantern.academy/api/'
 });
 
 export default axiosInstance;
